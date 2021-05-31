fbe144405b15d24744884192bd6daac0170b7f41
nvak1999 <76765929+nvak1999@users.noreply.github.com>
2021-05-31 19:45:46 +0700

    asdasd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./page/HomePage";
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../component/Header";
+import Body from "../component/Body";
+const HomePage = () => {
+  return;
+  <div>
+    <Header />
+    <Body />
+  </div>;
+};
+
+export default HomePage;
